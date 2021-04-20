656a44750cdc750a3ae1327e4312f77f58b1a355
acerslee <acerslee@DESKTOP-L3FISQG.localdomain>
2021-04-20 17:38:02 -0400

    Commti before push

---
+++ src/archives/projects.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import Header from '../components/Header.js';
+// import AllProjects from '../components/AllProjects.js';
+// import Seo from '../components/SEO.js';
+
+// const ProjectsPage = () => (
+//   <div className = 'projects-page-container'>
+//     <Seo title = 'Projects' description = 'about projects' />
+//     <Header />
+//     <AllProjects />
+//   </div>
+// );
+
+// export default ProjectsPage;
