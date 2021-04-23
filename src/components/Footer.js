41760ddb73b4f79f7b4baa6d506a6cf12a204eba
acerslee <acerslee@DESKTOP-L3FISQG.localdomain>
2021-04-23 16:49:30 -0400

    Replaced scroll-to-top with button in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import data from '../data/socialLinks.js';
 import { FaChevronCircleUp } from 'react-icons/fa';
+import { document } from 'browser-monads';
 import styled from 'styled-components';
 
 const date = new Date().getFullYear();
@@ -10,8 +11,7 @@ const FooterContainer = styled.div`
   background-color: #0d1957;
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
 `;
 
 // const Header = styled.h1`
@@ -22,27 +22,18 @@ const FooterContainer = styled.div`
 //   }
 // `;
 
-const FooterLinks = styled.div`
-  width: 50%;
+const FooterLinks = styled.footer`
+  width: 30%;
   display: flex;
   justify-content: space-evenly;
   margin: 2rem auto 2rem auto;
 `;
 
 const Copyright = styled.h6`
+  width: 30%;
   font-size: 1.6vh;
   text-align: left;
   color: #edede8;
-  @media(max-width: 700px){
-    font-size: 1.5vh;
-  }
-`;
-
-const ReturnLink = styled.a`
-  font-size: 1.3vh;
-  text-align: center;
-  color: black;
-  text-decoration: none !important;
   @media(max-width: 700px){
     font-size: 1.5vh;
   }
@@ -61,12 +52,34 @@ const renderData = data.map(datalink => {
   )
 });
 
+
 const Footer = () => {
+
+  var target = document.querySelector('footer');
+  var scrollToTopBtn = document.querySelector('.showBtn');
+  var rootElement = document.documentElement;
+
+  const callback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) scrollToTopBtn.classList.add('showBtn')
+      else scrollToTopBtn.classList.remove('showBtn')
+    })
+  };
+
+  useEffect(() => {
+    let observer = new IntersectionObserver(callback);
+    observer.observe(target);
+  }, [])
+
+
+  const scrollToTop = () => {
+    rootElement.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  };
+
   return(
-    <>
-      <ReturnLink href = '#navbar'>
-        Back to top
-      </ReturnLink>
       <FooterContainer id = 'closer'>
       <Copyright>
           ©{date} Alex Lee
@@ -74,8 +87,12 @@ const Footer = () => {
         <FooterLinks>
           {renderData}
         </FooterLinks>
+        <FaChevronCircleUp
+          className = 'showBtn'
+          style = {{color: 'white'}}
+          onClick = {scrollToTop}
+        />
       </FooterContainer>
-    </>
   );
 }
 
