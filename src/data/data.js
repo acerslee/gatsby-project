445bff3d86b10aca4dfa017ed04da05d3fec3ee1
Alex Lee <ahjlee712@gmail.com>
2021-06-21 15:02:48 -0400

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -9,8 +9,7 @@ const languageList = [
   'Node.js & Express',
   'SQL (MySQL, PostgreSQL)',
   'MongoDB (Mongoose)',
-  'React.js',
-  'Gatsby.js'
+  'Frontend Frameworks (React, Gatsby, Next)',
 ];
 
 const listlinks = [
