74a03cdf668e4ada541d607e78cf09f48083e112
Alex Lee <ahjlee712@gmail.com>
2023-03-06 20:22:23 -0500

    changed starter color to black

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
-const dotenv = require('dotenv');
-if (process.env.NODE_ENV !== 'production') dotenv.config()
+const dotenv = require("dotenv")
+if (process.env.NODE_ENV !== "production") dotenv.config()
 
 module.exports = {
-  flags:{
+  flags: {
     FAST_DEV: true,
     PARALLEL_SOURCING: true,
   },
   siteMetadata: {
     title: "Alex Lee Portfolio",
-    description: "I'm a Software Engineer based out of NYC with a passion in photography and coffee. Check out more of my contents in here.",
+    description:
+      "I'm a Software Engineer based out of NYC with a passion in photography and coffee. Check out more of my contents in here.",
     author: "Alex Lee",
     siteUrl: "https://alexanderhjlee.com",
-    twitterUsername: "@acerslee"
+    twitterUsername: "@acerslee",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
@@ -25,17 +26,17 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `roboto condensed\:300,400,400i,700` // you can also specify font weights and styles
+          `roboto condensed\:300,400,400i,700`, // you can also specify font weights and styles
         ],
-        display: 'swap'
-      }
+        display: "swap",
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
         path: `${__dirname}/src/data/`,
-      }
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
@@ -50,11 +51,11 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/data/images/favicon/favicon-16x16.png`, // This path is relative to the root of the site.
       },
     },
   ],
-};
+}
